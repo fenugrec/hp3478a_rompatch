264fb4c2ba180e6b5cb8112d16ab918772c061e6
fenugrec <fenugrec@users.sourceforge.net>
2020-11-02 13:21:25 -0500

    patchrom : add offset arg

--- patchrom.sh
+++ patchrom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#run as "patchrom <orig.bin> <patch.hex> <out.bin>
-srec_cat $1 -bin $2 -intel -contradictory-bytes=ignore -redundant-bytes=ignore -o temp.bin -bin
-./ckfix temp.bin $3
+#run as "patchrom <orig.bin> <patch.hex> <patchoffset> <out.bin>
+srec_cat $1 -bin $2 -intel -offset $3 -contradictory-bytes=warn -redundant-bytes=ignore -o temp.bin -bin
+./ckfix temp.bin $4
 rm temp.bin
