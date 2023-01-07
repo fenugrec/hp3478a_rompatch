b916fb71bc6fe9341f61082a633b985c50bf6a3f
fenugrec <fenugrec@users.sourceforge.net>
2023-01-07 18:17:35 -0500

    quiet down patchrom.sh

--- patchrom.sh
+++ patchrom.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #run as "patchrom <orig.bin> <patch.hex> <patchoffset> <out.bin>
-srec_cat $1 -bin $2 -intel -offset $3 -contradictory-bytes=warn -redundant-bytes=ignore -o temp.bin -bin
+#
+# for debugging patches : try -contradictory-bytes=ignore
+srec_cat $1 -bin $2 -intel -offset $3 -contradictory-bytes=ignore -redundant-bytes=ignore -o temp.bin -bin
 ./ckfix temp.bin $4
 rm temp.bin
